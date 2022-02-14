c64b88c01a3ed35707aa4171d68d315abe0d1895
Samuele Leli <s1084424@studenti.univpm.it>
2022-02-14 10:21:31 +0100

    Eliminazione file install-lib.sh

--- install-lib.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "Installing Project dependencies.."
-cd Truffle/ && npm i && npm install -g truffle@5.3.14
-echo "Setting client"
-cd ../Client && npm i
-echo "Installazione avvenuta con successo!"
